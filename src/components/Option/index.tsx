a612eed8dcbeb5faed22e0c4f57220bf69c70c43
Excession27 <feboobef27@gmail.com>
2022-08-10 15:54:43 +0200

    Refined animations

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -35,41 +35,44 @@ const OptionStep = ({ title, optionsArray, id }: Props) => {
   console.log(chosen, id);
   return (
     <>
-      {/* <Collapse
-        in={id === chosen}
-        transitionDuration={500}
-        transitionTimingFunction="linear"
-      > */}
-      <div
-        className={`option ${chosen < id && "disabled"}`}
-        onClick={() => {
-          setSelected({ isSet: false, text: "" });
-
-          if (chosen > id) {
-            setChosen(id);
-          }
-        }}
+      <Transition
+        mounted={chosen === id || chosen > id}
+        transition="scale-y"
+        duration={400}
+        timingFunction="ease"
       >
-        <div className="title__picked">
-          <h2 className="option__title">{title}</h2>{" "}
-          <Transition
-            mounted={selected.text.length > 0}
-            transition="scale-x"
-            duration={400}
-            timingFunction="ease"
-          >
-            {(style) => <p style={style}>{selected.text}</p>}
-          </Transition>
-        </div>
+        {(styleDiv) => (
+          <div
+            style={styleDiv}
+            className={`option ${chosen < id && "disabled"}`}
+            onClick={() => {
+              setSelected({ isSet: false, text: "" });
 
-        <Transition
-          mounted={id === chosen}
-          transition="fade"
-          duration={600}
-          timingFunction="ease"
-        >
-          {(style) => (
-            <div style={style}>
+              if (chosen > id) {
+                setChosen(id);
+              }
+            }}
+          >
+            <div className="title__picked">
+              <h2 className="option__title">{title}</h2>{" "}
+              <Transition
+                mounted={selected.text.length > 0}
+                transition="scale-x"
+                duration={400}
+                timingFunction="ease"
+              >
+                {(style) => <p style={style}>{selected.text}</p>}
+              </Transition>
+            </div>
+
+            {/* <Transition
+              mounted={id === chosen}
+              transition="fade"
+              duration={600}
+              timingFunction="ease"
+            >
+              {(style) => ( */}
+            <div>
               {chosen === id &&
                 optionsArray.map((option, index) => (
                   <div key={index} className="option__choices">
@@ -84,10 +87,9 @@ const OptionStep = ({ title, optionsArray, id }: Props) => {
                   </div>
                 ))}
             </div>
-          )}
-        </Transition>
-      </div>
-      {/* </Collapse> */}
+          </div>
+        )}
+      </Transition>
     </>
   );
 };
